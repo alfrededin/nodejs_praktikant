cffbeca480b79c1381469cdcfe231886b90c02ab
alfrededin <alfrededin@gmail.com>
2020-12-04 09:31:24 +0100

    final version

--- web_server.js
+++ web_server.js
@@ -1,56 +1,57 @@
-const Koa = require("koa");
-const Router = require("koa-router");
-const axios = require("axios");
+const Koa = require("koa")
+const Router = require("koa-router")
+const axios = require("axios")
 // Prometheus client
-const prom = require("prom-client");
-const e = require("express");
-const { runInContext } = require("vm");
+const prom = require("prom-client")
 // Init default metrics collection
 // const collectDefaultMetrics = prom.collectDefaultMetrics;
 // collectDefaultMetrics();
 // create Server instance
-const app = new Koa();
-const router = new Router();
-console.log(process.version);
+const app = new Koa()
+const router = new Router()
 
-// const req_duration = new prom.Gauge({
-//     name: 'request duration',
-//     help: 'Ammount of milliseconds the request took'
-// })
+const req_duration = new prom.Gauge({
+  name: "request_duration",
+  help: "Ammount of milliseconds the request took",
+})
 
 const req_counter = new prom.Counter({
   name: "requests_recieved",
   help: "number of requests recieved",
-});
-
-// req_duration.setToCurrentTime();
-// const end = req_duration.startTimer();
+})
 
 // Set up the routes
 router.get("/", (ctx) => {
-  req_counter.inc();
-  ctx.body = `HELLO WORLD!`;
-});
+  req_counter.inc()
+  ctx.body = `HELLO WORLD!`
+})
 
 router.get("/aw", async (ctx) => {
-  req_counter.inc();
+  req_counter.inc()
+  const end = req_duration.startTimer()
   const result = await axios
     .get("http://www.academicwork.se")
     .then((x) => x.data)
-    .catch((error) => console.log(error));
-  console.log(result);
+    .catch((error) => console.log(error))
+  console.log(result)
+  end()
+  ctx.body = result
+})
 
-  ctx.body = result;
-});
+router.get("/health", (ctx) => {
+  req_counter.inc()
+  ctx.body = `Im healthy ${Date.now}`
+})
 
 router.get("/metrics", (ctx) => {
-  req_counter.inc()(ctx);
-  ctx.body = prom.register.metrics();
-});
-const run = async () => {
-  app.use(router.routes());
-  app.listen(3000);
-  console.log("listening on port 3000");
-}; // Tell Koa to use the routes
+  req_counter.inc()
+  ctx.body = prom.register.metrics()
+})
 
-run();
+// ternary
+// if true ? use_me : else_me
+const port = process.env.SERVICE_PORT ? process.env.SERVICE_PORT : 3000
+
+app.use(router.routes())
+app.listen(port)
+console.log(`listening on port ${port}`)
